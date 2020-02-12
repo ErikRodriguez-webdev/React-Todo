3e9d28f6b189a0d39203e51e8edef2ed09879dd6
Erik Rodriguez <eric19.21.03@gmail.com>
2020-02-11 16:04:30 -0800

    Add style to completed toDo
    Easier for user to see feedback on what ToDo is selected when being
    completed.

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./Todo.css";
 
 function Todo(props) {
   //console.log("In todo.js props: ", props);
   // Displaying tasks here
 
   return (
-    <ul onClick={() => props.toggleComplete(props.todoData.id)}>
+    <ul
+      onClick={() => props.toggleComplete(props.todoData.id)}
+      className={`${props.todoData.completed ? "todoStyle" : ""}`}
+    >
       <li>{props.todoData.task}</li>
     </ul>
   );
